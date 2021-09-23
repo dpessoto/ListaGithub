fc261b6b99c0aac6e17bd47cc4d155782dc5de40
Daniel Pessoto <d.pessoto@hotmail.com>
2021-09-23 16:42:55 -0300

    start ListRepositoriesActivity

--- app/src/main/java/pessoto/android/mobile/challenge/listagithub/feature/main/MainActivity.kt
+++ app/src/main/java/pessoto/android/mobile/challenge/listagithub/feature/main/MainActivity.kt
@@ -1,12 +1,20 @@
 package pessoto.android.mobile.challenge.listagithub.feature.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import pessoto.android.mobile.challenge.listagithub.R
+import pessoto.android.mobile.challenge.listagithub.feature.listRepositories.ListRepositoriesActivity
 import pessoto.android.mobile.challenge.listagithub.util.view.BaseActivity
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Handler().postDelayed({
+            startActivity(Intent(this, ListRepositoriesActivity::class.java))
+            finish()
+        }, 3000)
     }
 }
