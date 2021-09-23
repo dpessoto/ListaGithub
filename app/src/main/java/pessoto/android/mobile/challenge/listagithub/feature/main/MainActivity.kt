7d6e54064efee67c7e5df9d35c83fd1460ec6f3e
Daniel Pessoto <d.pessoto@hotmail.com>
2021-09-23 16:35:17 -0300

    add BaseActivity.kt

---
+++ app/src/main/java/pessoto/android/mobile/challenge/listagithub/feature/main/MainActivity.kt
@@ -0,0 +1,12 @@
+package pessoto.android.mobile.challenge.listagithub.feature.main
+
+import android.os.Bundle
+import pessoto.android.mobile.challenge.listagithub.R
+import pessoto.android.mobile.challenge.listagithub.util.view.BaseActivity
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/pessoto/android/mobile/challenge/listagithub/util/view/BaseActivity.kt
@@ -0,0 +1,50 @@
+package pessoto.android.mobile.challenge.listagithub.util.view
+
+import android.annotation.SuppressLint
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import pessoto.android.mobile.challenge.listagithub.R
+
+open class BaseActivity : AppCompatActivity() {
+
+    private lateinit var layout: ConstraintLayout
+    private var goingToBackground = true
+
+    @SuppressLint("InflateParams")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        layout = layoutInflater.inflate(R.layout.activity_base, null) as ConstraintLayout
+
+        supportActionBar?.hide()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        goingToBackground = true
+        try {
+            (window.decorView as ViewGroup).removeView(layout)
+        } catch (e: Exception) {
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        try {
+            if (goingToBackground)
+                (window.decorView as ViewGroup).addView(layout)
+        } catch (e: Exception) {
+        }
+    }
+
+    override fun finish() {
+        super.finish()
+        goingToBackground = false
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        goingToBackground = false
+    }
+}
